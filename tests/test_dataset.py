3a6f770b32eda96e80d28bae4886f0e072994a9d
chinise <xml422@gmail.com>
2025-02-24 19:24:19 +0100

    Use mocker fixtures for dataset tests

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
-from unittest import mock
+from typing import TYPE_CHECKING
 
 import pandas as pd
 import pytest
@@ -9,8 +9,11 @@ import pytest
 from ilthermoml.dataset import Entry
 from ilthermoml.exceptions import EntryError
 
+if TYPE_CHECKING:
+    from pytest_mock import MockerFixture
 
-def test_retrive_entry() -> None:
+
+def test_retrive_entry(mocker: MockerFixture) -> None:
     # Arrange
     @dataclass()
     class MockIlthermopyEntry:
@@ -18,25 +21,24 @@ def test_retrive_entry() -> None:
         data: pd.DataFrame
 
     result = pd.DataFrame({"Replacement": [10]})
-
-    with mock.patch(
+    mock_get_entry = mocker.patch(
         "ilthermopy.GetEntry",
         return_value=MockIlthermopyEntry(
             header={"To_be_replaced": "Replacement"},
             data=pd.DataFrame({"To_be_replaced": [10]}),
         ),
-    ) as mock_get_entry:
-        entry = Entry("id")
+    )
+
+    # Act
+    entry = Entry("id")
 
     # Assert
     mock_get_entry.assert_called_once_with("id")
     assert entry.data.equals(result)
 
 
-def test_failed_to_retrive_entry() -> None:
+def test_failed_to_retrive_entry(mocker: MockerFixture) -> None:
     # Assert
-    with (
-        mock.patch("ilthermopy.GetEntry", mock.Mock(side_effect=Exception)),
-        pytest.raises(EntryError),
-    ):
+    mocker.patch("ilthermopy.GetEntry", mocker.Mock(side_effect=Exception))
+    with pytest.raises(EntryError):
         Entry("invalidID")
