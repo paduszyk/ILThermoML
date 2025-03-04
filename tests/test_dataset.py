04e2fd43adcdfc0d92bf9773ad0c48b676611986
chinise <xml422@gmail.com>
2025-03-04 14:35:52 +0100

    Add Dataset class tests

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -5,7 +5,7 @@ from typing import TYPE_CHECKING, Any
 import pandas as pd
 import pytest
 
-from ilthermoml.dataset import Entry
+from ilthermoml.dataset import Dataset, Entry
 from ilthermoml.exceptions import EntryError
 
 if TYPE_CHECKING:
@@ -60,3 +60,74 @@ def test_entry_updates_ilthermo_entry_data_columns_with_header(
 
     # Assert.
     assert entry.data.columns == ["mock_header"]
+
+
+def test_entry_attempts_to_prepare_entry(mocker: MockerFixture) -> None:
+    # Mock.
+    mocker.patch("ilthermopy.GetEntry")
+    mock_dataset = mocker.Mock()
+
+    # Act.
+    Entry("mock_id", mock_dataset)
+
+    # Assert.
+    mock_dataset.prepare_entry.assert_called_once()
+
+
+class DatasetTest(Dataset):
+    @staticmethod
+    def get_entry_ids() -> list[str]:
+        return ["mock_id"]
+
+    @staticmethod
+    def prepare_entry(entry: Entry) -> None:
+        _ = entry
+
+
+@pytest.fixture
+def dataset() -> DatasetTest:
+    return DatasetTest()
+
+
+def test_dataset_attempts_to_retrive_ids(
+    dataset: DatasetTest, mocker: MockerFixture
+) -> None:
+    # Mock.
+    mock_get_entry_ids = mocker.patch.object(dataset, "get_entry_ids", return_value=[])
+
+    # Act.
+    dataset.populate()
+
+    # Assert.
+    mock_get_entry_ids.assert_called_once()
+
+
+def test_dataset_attempts_to_retrive_entries(
+    dataset: DatasetTest, mocker: MockerFixture
+) -> None:
+    # Mock.
+    mock_entry = mocker.patch("ilthermoml.dataset.Entry", return_value="Mock_entry")
+
+    # Act.
+    dataset.populate()
+
+    # Assert.
+    assert dataset.entries[0] == "Mock_entry"  # type: ignore[comparison-overlap]
+    mock_entry.assert_called_once()
+
+
+def test_dataset_skips_on_entryerror(
+    dataset: DatasetTest, mocker: MockerFixture
+) -> None:
+    # Mock.
+    class MockError(Exception):
+        pass
+
+    def mock_get_entry(code: str) -> Any:  # noqa: ANN401
+        if code == "mock_id":
+            raise MockError
+
+    mocker.patch("ilthermopy.GetEntry", side_effect=mock_get_entry)
+
+    # Act & Assert.
+    dataset.populate()
