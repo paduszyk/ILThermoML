058111baefecdb27ecebfddac0ce085ed18682fe
Adrian Racki <adrian.racki.1@gmail.com>
2025-03-12 14:09:32 +0000

    Add env variable JOBLIB_CACHE_VERBOSITY

--- src/ilthermoml/memory.py
+++ src/ilthermoml/memory.py
@@ -1,4 +1,8 @@
+import os
+
 from joblib import Memory
 
-ilt_memory = Memory(location=".ilt_cache", verbose=0)
+ilt_memory = Memory(
+    location=".ilt_cache", verbose=int(os.getenv("JOBLIB_CACHE_VERBOSITY", "0"))
+)
 """Memory object for caching ILThermo data."""
