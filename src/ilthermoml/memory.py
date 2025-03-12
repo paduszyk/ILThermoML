72e7ef0100c59bae485dac9dea03b7d792ce1d09
Kamil Paduszyński <paduszyk@gmail.com>
2025-03-12 15:28:58 +0100

    Reformat to satisfy Ruff's COM812

--- src/ilthermoml/memory.py
+++ src/ilthermoml/memory.py
@@ -3,6 +3,7 @@ import os
 from joblib import Memory
 
 ilt_memory = Memory(
-    location=".ilt_cache", verbose=int(os.getenv("JOBLIB_CACHE_VERBOSITY", "0"))
+    location=".ilt_cache",
+    verbose=int(os.getenv("JOBLIB_CACHE_VERBOSITY", "0")),
 )
 """Memory object for caching ILThermo data."""
