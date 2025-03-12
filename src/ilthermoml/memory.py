9dc7338e77a7728aafd0706937ce37f8ea293384
Adrian <115142195+AdrianRacki@users.noreply.github.com>
2025-03-12 15:33:19 +0100

    Enable silent joblib cache logging via environment variable (#75)

--- src/ilthermoml/memory.py
+++ src/ilthermoml/memory.py
@@ -1,4 +1,9 @@
+import os
+
 from joblib import Memory
 
-ilt_memory = Memory(location=".ilt_cache")
+ilt_memory = Memory(
+    location=".ilt_cache",
+    verbose=int(os.getenv("JOBLIB_CACHE_VERBOSITY", "0")),
+)
 """Memory object for caching ILThermo data."""
