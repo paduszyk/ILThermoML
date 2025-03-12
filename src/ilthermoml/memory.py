79c049c1e3c1a132d4c44ac94c83b55215df9858
Adrian Racki <adrian.racki.1@gmail.com>
2025-03-12 14:07:46 +0000

    Revert "Add env variable JOBLIB_CACHE_VERBOSITY"
    This reverts commit 9b0b0edb84cd19dae70459e9e0a760e620fe67b7.

--- src/ilthermoml/memory.py
+++ src/ilthermoml/memory.py
@@ -1,10 +1,4 @@
-import os
-
-from dotenv import load_dotenv
 from joblib import Memory
 
-load_dotenv()
-cache_verbosity = os.getenv("JOBLIB_CACHE_VERBOSITY", "0")
-
-ilt_memory = Memory(location=".ilt_cache", verbose=int(cache_verbosity))
+ilt_memory = Memory(location=".ilt_cache", verbose=0)
 """Memory object for caching ILThermo data."""
