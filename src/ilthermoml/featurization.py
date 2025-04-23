2b33d2c081bd8ac43d34afef2078ff383a8543e7
akidec <54854740+chinise@users.noreply.github.com>
2025-04-23 12:08:41 +0200

    Add salt featurizer (#93)

--- src/ilthermoml/featurization.py
+++ src/ilthermoml/featurization.py
@@ -1,10 +1,11 @@
 from abc import ABC, abstractmethod
+from collections.abc import Callable
 from typing import Any
 
 import padelpy  # type: ignore [import-untyped]
 from rdkit.Chem.Descriptors import CalcMolDescriptors
 
-from .chemistry import Molecule
+from .chemistry import Molecule, Salt
 from .exceptions import FeaturizerError
 from .memory import ilt_memory
 
@@ -45,4 +46,54 @@ class PadelMoleculeFeaturizer(MoleculeFeaturizer):
     """Molecule featurizer class for calculating descriptors using padel."""
 
     def _featurize(self, molecule: Molecule) -> dict[str, Any]:
-        return padel_calc_descriptors(molecule.smiles)  # type: ignore [no-any-return]
+        descriptors = padel_calc_descriptors(molecule.smiles)
+        for key, value in descriptors.items():
+            try:
+                descriptors[key] = float(value)
+            except ValueError:
+                continue
+
+        return descriptors  # type: ignore [no-any-return]
+
+
+class CachingMoleculeFeaturizer(MoleculeFeaturizer):
+    """Wrapper molecule featurizer class that caches calculated descriptors."""
+
+    def __init__(self, featurizer: MoleculeFeaturizer) -> None:
+        self._inner_featurize = featurizer
+        self._cache: dict[str, dict[str, Any]] = {}
+
+    def _featurize(self, molecule: Molecule) -> dict[str, Any]:
+        if molecule.smiles not in self._cache:
+            self._cache[molecule.smiles] = self._inner_featurize(molecule)
+        return self._cache[molecule.smiles]
+
+
+class SaltFeaturizer:
+    """Class for calculating salt descriptors."""
+
+    def __init__(
+        self,
+        combination_rule: Callable[[Any, Any], Any],
+        featurize: MoleculeFeaturizer,
+    ) -> None:
+        self.combination_rule = combination_rule
+        self.featurize = CachingMoleculeFeaturizer(featurize)
+
+    def __call__(self, salt: Salt) -> dict[str, Any]:
+        cation_features = self.featurize(salt.cation)
+        anion_features = self.featurize(salt.anion)
+
+        output_keys = set(list(cation_features) + list(anion_features))
+        output_values: list[Any] = []
+
+        for key in output_keys:
+            if not cation_features[key] or not anion_features[key]:
+                output_values.append(None)
+                continue
+
+            output_values.append(
+                self.combination_rule(cation_features[key], anion_features[key])
+            )
+
+        return dict(zip(output_keys, output_values, strict=True))
--- tests/test_featurization.py
+++ tests/test_featurization.py
@@ -1,15 +1,17 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Any
 
 import pytest
 
-from ilthermoml.chemistry import Ion
+from ilthermoml.chemistry import Ion, Salt
 from ilthermoml.exceptions import FeaturizerError
 from ilthermoml.featurization import (
+    CachingMoleculeFeaturizer,
     MoleculeFeaturizer,
     PadelMoleculeFeaturizer,
     RDKitMoleculeFeaturizer,
+    SaltFeaturizer,
 )
 
 if TYPE_CHECKING:
@@ -75,3 +77,92 @@ def test_padel_molecule_featurizer_attempts_calculating_descriptors(
 
     # Assert
     mock_calc_descriptors.assert_called_once_with(molecule.smiles)
+
+
+def test_padel_molecule_featurizer_converts_output_to_floats(
+    mocker: MockerFixture,
+) -> None:
+    # Mock.
+    mocker.patch(
+        "ilthermoml.featurization.padel_calc_descriptors",
+        return_value={
+            "TestConvertable": "0.15",
+            "TestUnconvertable": "Unconvertable",
+        },
+    )
+
+    # Arrange.
+    featurize = PadelMoleculeFeaturizer()
+    molecule = Ion("[Na+]")
+
+    # Act.
+    desctriptors = featurize(molecule)
+
+    # Assert
+    assert type(desctriptors["TestConvertable"]) is float
+    assert type(desctriptors["TestUnconvertable"]) is str
+
+
+def test_cahing_molecule_featurizer_calls_inner_featurizer_only_once(
+    mocker: MockerFixture,
+) -> None:
+    # Spy.
+    spy_featurizer = mocker.spy(RDKitMoleculeFeaturizer(), "__call__")
+
+    # Arrange.
+    featurize = CachingMoleculeFeaturizer(spy_featurizer)
+    molecule = Ion("[Na+]")
+
+    # Act.
+    featurize(molecule)
+    featurize(molecule)
+
+    # Assert
+    spy_featurizer.assert_called_once_with(molecule)
+
+
+def test_salt_featurizer_calculates_values_using_combination_rule_and_featurizer(
+    mocker: MockerFixture,
+) -> None:
+    # Spy.
+    def test_rule(x: Any, y: Any) -> Any:
+        return (x + y) / 2
+
+    spy_rule = mocker.spy(test_rule, "__call__")
+
+    # Mock.
+    mock_featurizer = mocker.MagicMock(return_value={"Test": 1.0})
+
+    # Arrange.
+    featurize = SaltFeaturizer(spy_rule, mock_featurizer)
+
+    ions_in_salt = 2
+    descriptors_per_ion = 1
+    salt = Salt("[Na+].[Cl-]")
+
+    # Act.
+    featurize(salt)
+
+    # Assert.
+    assert spy_rule.call_count == descriptors_per_ion
+    assert mock_featurizer.call_count == ions_in_salt
+
+
+def test_salt_featurizer_value_is_none_when_descriptor_is_none() -> None:
+    # Arrange.
+    def test_rule(x: Any, y: Any) -> Any:
+        return (x + y) / 2
+
+    def test_get_descriptors(molecule: Molecule) -> dict[str, Any]:
+        if molecule.smiles == "[Na+]":
+            return {"Test": 1.0, "Other": 1.0}
+        if molecule.smiles == "[Cl-]":
+            return {"Test": None, "Other": 1.0}
+
+        return {"Test": None, "Other": 1.0}
+
+    featurize = SaltFeaturizer(test_rule, test_get_descriptors)  # type: ignore [arg-type]
+    salt = Salt("[Na+].[Cl-]")
+
+    # Act & assert.
+    assert not featurize(salt)["Test"]
